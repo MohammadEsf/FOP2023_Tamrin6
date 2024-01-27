1ddb8552892d4caf0dd978116f34773891fe512a
Kiarash <kiarash2004sh@gmail.com>
2024-01-27 11:03:20 -0800

    removed garbage files

--- soal 1 (1).py
+++
@@ -1,8 +0,0 @@
-a= int(input())
-b= int(input())
-c= bin(a ^ b)
-count = 0
-for i in c:
-    if i=='1':
-        count +=1
-print(count)
--- soal 1 (1)j.py
+++
@@ -1,9 +0,0 @@
-n = int(input())
-def print_pascal(n):
-    for line in range(1, n + 1):
-        c = 1
-        for i in range(1, line + 1):
-            print(c, end=" ")
-            c = c * (line - i) // i
-        print()
-print_pascal(n)
